b0ebaa30913e3e8f2452fe009b76ea2db651aa73
hulei <hulei@nonobank.com>
2018-04-17 11:09:04 +0800

    key

--- react-webpack/src/sw.js
+++ react-webpack/src/sw.js
@@ -15,7 +15,7 @@ self.addEventListener('install', function (event) {
 self.addEventListener('activate', function (event) {
     event.waitUntil(caches.keys().then(function (cacheNames) {
         return Promise.all(cacheNames.map(function (cacheName) {
-            if(cacheName.indexOf('dxy')>-1){
+            if(cacheName.indexOf('nono')>-1){
                 return caches.delete(cacheName);
             }
         }));
