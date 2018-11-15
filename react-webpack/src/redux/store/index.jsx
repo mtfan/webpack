6262ef489864d165465679956a8052d988f33d30
hulei <hulei@nonobank.com>
2018-11-15 18:16:29 +0800

    persist

--- react-webpack/src/redux/store/index.jsx
+++ react-webpack/src/redux/store/index.jsx
@@ -1,7 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import storage from 'redux-persist/lib/storage'; //localStorage
+import storageSession from 'redux-persist/lib/storage/session'; //sessionStorage
+// import { AsyncStorage } from 'react-native'; //react-native Storage
 import rootReducer from '../reducers';
 
 const persistConfig = {
