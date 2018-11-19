06f4959159ee25ee12fe0914691c6d621df6eb4c
hulei <hulei@nonobank.com>
2018-11-19 12:54:36 +0800

    第三方集成

--- vue-ssr-nuxt/nuxt.config.js
+++ vue-ssr-nuxt/nuxt.config.js
@@ -1,5 +1,3 @@
-const pkg = require('./package');
-
 module.exports = {
   mode: 'universal',
 
@@ -7,13 +5,26 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: pkg.name,
+    title: 'vue ssr nuxt',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      {
+        name: 'viewport',
+        content:
+          'initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no, width=device-width, viewport-fit=cover'
+      },
+      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'apple-mobile-web-app-capable', content: 'yes' },
+      {
+        id: 'apple-mobile-web-app-title',
+        name: 'apple-mobile-web-app-title',
+        content: 'vue'
+      },
+      { name: 'format-detection', content: 'telephone=no' },
+      { hid: 'description', name: 'description', content: 'vue' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'apple-touch-icon', href: '/favicon.ico' }]
   },
 
   /*
@@ -24,28 +35,32 @@ module.exports = {
   /*
    ** Global CSS
    */
-  css: [{ src: '~assets/sass/common/_base.scss', lang: 'scss' }],
+  css: [
+    'mint-ui/lib/style.css',
+    { src: '~assets/sass/common/_base.scss', lang: 'scss' }
+  ],
 
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [{ src: '~plugins/mint-ui' }],
 
   /*
    ** Nuxt.js modules
    */
-  modules: [],
+  modules: ['@nuxtjs/style-resources'],
 
   /*
    ** Build configuration
    */
   build: {
+    vendor: ['mint-ui'],
+    styleResources: {
+      scss: './assets/sass/common/_variables.scss'
+    },
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {},
-    styleResources: {
-      scss: './assets/sass/common/_variables.scss'
-    }
+    extend(config, ctx) {}
   }
 };
--- vue-ssr-nuxt/pages/index.vue
+++ vue-ssr-nuxt/pages/index.vue
@@ -12,6 +12,7 @@
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
         <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
+      <mt-button type="default">default</mt-button>
     </div>
   </section>
 </template>
