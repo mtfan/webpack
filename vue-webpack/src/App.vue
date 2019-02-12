8f1080399768e62ffbe6bc2844a73d58709a3423
黄梦莹 <523387582@qq.com>
2019-02-12 19:32:36 +0800

    动画

--- vue-webpack/src/App.vue
+++ vue-webpack/src/App.vue
@@ -2,18 +2,32 @@
   <div id="app">
     <!--开启缓存-->
     <keep-alive>
-      <router-view></router-view>
+      <transition name="fade" mode="out-in">
+        <router-view></router-view>
+      </transition>
     </keep-alive>
     <tab-bar></tab-bar>
   </div>
 </template>
 
 <script>
-  import TabBar from 'components/tab-bar'
+  import TabBar from "components/tab-bar";
 
   export default {
     components: {
       TabBar
     }
-  }
+  };
 </script>
+<style scoped>
+  /*淡入淡出*/
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 1s;
+  }
+
+  .fade-enter,
+  .fade-leave-to {
+    opacity: 0;
+  }
+</style>
--- vue-webpack/src/components/tab-bar/index.vue
+++ vue-webpack/src/components/tab-bar/index.vue
@@ -2,23 +2,23 @@
   <mt-tabbar v-model="selected">
     <mt-tab-item id="selected_home">
       <router-link to="/home" class="router-item">
-        <img class="tab-bar-icon" src="../../static/images/tabs/home-on.png" v-if="selected=='selected_home'"/>
-        <img class="tab-bar-icon" src="../../static/images/tabs/home-off.png" v-else/>
+        <img class="tab-bar-icon" src="../../static/images/tabs/home-on.png" v-if="selected=='selected_home'" />
+        <img class="tab-bar-icon" src="../../static/images/tabs/home-off.png" v-else />
         <span>首页</span>
       </router-link>
-      
+
     </mt-tab-item>
     <mt-tab-item id="selected_found">
       <router-link to="/found" class="router-item">
-        <img class="tab-bar-icon" src="../../static/images/tabs/found-on.png" v-if="selected=='selected_found'"/>
-        <img class="tab-bar-icon" src="../../static/images/tabs/found-off.png" v-else/>
+        <img class="tab-bar-icon" src="../../static/images/tabs/found-on.png" v-if="selected=='selected_found'" />
+        <img class="tab-bar-icon" src="../../static/images/tabs/found-off.png" v-else />
         <span>发现</span>
       </router-link>
     </mt-tab-item>
     <mt-tab-item id="selected_mine">
       <router-link to="/mine" class="router-item">
-        <img class="tab-bar-icon" src="../../static/images/tabs/mine-on.png" v-if="selected=='selected_mine'"/>
-        <img class="tab-bar-icon" src="../../static/images/tabs/found-off.png" v-else/>
+        <img class="tab-bar-icon" src="../../static/images/tabs/mine-on.png" v-if="selected=='selected_mine'" />
+        <img class="tab-bar-icon" src="../../static/images/tabs/mine-off.png" v-else />
         <span>我的</span>
       </router-link>
     </mt-tab-item>
@@ -26,30 +26,32 @@
 </template>
 
 <script type="text/ecmascript-6">
-export default {
-  data() {
-    return {
-      selected: 'selected_home'
-    };
-  }
-};
+  export default {
+    data() {
+      return {
+        selected: 'selected_home'
+      };
+    }
+  };
 </script>
 
 <style lang="scss">
-.router-item {
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  height: 100%;
-  align-items: center;
-  justify-content: center;
-}
-.tab-bar-icon {
-  width: 24px;
-  height: 24px;
-  margin-bottom: 5px;
-}
-.router-link-active {
-  color: rgb(254, 84, 109);
-}
+  .router-item {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .tab-bar-icon {
+    width: 24px;
+    height: 24px;
+    margin-bottom: 5px;
+  }
+
+  .router-link-active {
+    color: rgb(254, 84, 109);
+  }
 </style>
