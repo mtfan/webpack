3da1178f4c88026a0a883f413b7bbbac16235365
hulei <523387582@qq.com>
2019-04-13 18:25:13 +0800

    mock

--- vue-webpack/mock/app.js
+++ vue-webpack/mock/app.js
@@ -4,54 +4,52 @@ const path = require('path');
 const multer = require('multer');
 const app = express();
 const upload = multer({
-  dest: 'upload/'
+	dest: 'upload/',
 });
 
 app.use(bodyParser.json());
-app.use(express.static(resolve("../build")))
+app.use(express.static(resolve('../build')));
 
 function resolve(dir) {
-  return path.join(__dirname, dir);
+	return path.join(__dirname, dir);
 }
 
 app.get('/api/userInfo', (req, res) => {
-  res.json({
-    succeed: true,
-    errorCode: '0000000',
-    errorMessage: '成功',
-    data: {
-      username: 'hulei',
-      qq: '123456'
-    }
-  });
+	res.json({
+		succeed: true,
+		errorCode: '0000000',
+		errorMessage: '成功',
+		data: {
+			username: 'hulei',
+			qq: '123456',
+		},
+	});
 });
 
-
 app.post('/api/userInfo', (req, res) => {
-  res.json({
-    succeed: true,
-    errorCode: '0000000',
-    errorMessage: '成功',
-    data: null
-  });
+	res.json({
+		succeed: true,
+		errorCode: '0000000',
+		errorMessage: '成功',
+		data: null,
+	});
 });
 
 app.post('/api/upload', upload.single('file'), (req, res) => {
-  const file = req.file;
+	const file = req.file;
 
-  console.log('文件类型：%s', file.mimetype);
-  console.log('原始文件名：%s', file.originalname);
-  console.log('文件大小：%s', file.size);
-  console.log('文件保存路径：%s', file.path);
-  res.json({
-    succeed: true,
-    errorCode: '0000000',
-    errorMessage: '成功',
-    data: null
-  });
+	console.log('文件类型：%s', file.mimetype);
+	console.log('原始文件名：%s', file.originalname);
+	console.log('文件大小：%s', file.size);
+	console.log('文件保存路径：%s', file.path);
+	res.json({
+		succeed: true,
+		errorCode: '0000000',
+		errorMessage: '成功',
+		data: null,
+	});
 });
 
-
 app.listen(7878, () => {
-  console.log('listem on 7878 o(*￣︶￣*)o');
+	console.log('listem on 7878 o(*￣︶￣*)o');
 });
